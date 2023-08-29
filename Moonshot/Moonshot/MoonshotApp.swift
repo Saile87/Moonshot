25f15002b390578f4d6acfaa4df79fc29e0f0dc2
Elias Breitenbach <elias.b@me.com>
2023-08-29 22:25:37 +0200

    Ceate a project on github

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Elias Breitenbach on 29.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
