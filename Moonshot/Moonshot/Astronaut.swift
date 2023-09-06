b8e87045e007a2750f2943cd9bad319c075bd4f3
Elias Breitenbach <elias.b@me.com>
2023-09-06 20:37:43 +0200

    Loading a specific kind of Codable data

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Elias Breitenbach on 06.09.23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
